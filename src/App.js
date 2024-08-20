bce50713725ff285428e4de97fb110742674365b
Mohamad-Ajmal <Ajmalibrahimkhail121@gamil.com>
2024-08-20 10:03:35 +0430

    setup static design

---
+++ src/App.js
@@ -0,0 +1,91 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+
+export default function App(){
+  return (
+    <div className="app">
+      <div className="sidebar">
+      <FriendsList />
+      <FormAddFriend />
+      <Button>Add Friend</Button>
+      </div>
+     
+    </div>
+  )
+}
+
+function FriendsList(){
+
+  const friends = initialFriends;
+  return (
+    <ul>
+      {
+        friends.map((friend) => (
+          <Friend friend={friend} key={friend.id}/>
+        ))
+      }
+    </ul>
+  )
+}
+
+function Friend({friend}){
+  return <li>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+
+    {friend.balance < 0 && (
+      <p className="red">
+      You owe {friend.name} {Math.abs(friend.balance)}$
+      </p>
+    )}
+    {friend.balance > 0 && (
+      <p className="green">
+      {friend.name}  owe you {Math.abs(friend.balance)}$
+      </p>
+    )}
+    {friend.balance === 0 && (
+      <p>
+      You and {friend.name} are even
+      </p>
+    )}
+    <Button>Select</Button>
+  </li>
+
+}
+
+function Button({children}){
+  return <button className="button">{children}</button>
+}
+
+
+function FormAddFriend(){
+  return (
+    <form className="from-add-friend">
+      <label>Friend name</label>
+      <input type="text" />
+
+      <label>Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  )
+}
